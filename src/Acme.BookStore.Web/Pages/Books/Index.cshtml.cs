9e2c48330a952165ac26d9d1bdd8f9c44f1ce25e
Battle Patata <ovez.choliev@gmail.com>
2024-12-23 08:18:04 +0500

    Add project files.

---
+++ src/Acme.BookStore.Web/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Acme.BookStore.Web.Pages.Books;
+
+public class IndexModel : PageModel {
+    public void OnGet() {
+    }
+}
---
+++ test/Acme.BookStore.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Books/BookAppService_Tests.cs
@@ -0,0 +1,8 @@
+﻿using Acme.BookStore.Books;
+using Xunit;
+
+namespace Acme.BookStore.EntityFrameworkCore.Applications.Books;
+[Collection(BookStoreTestConsts.CollectionDefinitionName)]
+public class BookAppService_Tests 
+    : BookAppService_Tests<BookStoreEntityFrameworkCoreTestModule> {
+}
